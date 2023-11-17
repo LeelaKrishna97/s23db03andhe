29ab529c12a8ac52a0d43e08899a74463460b630
Leela Krishna <142929100+LeelaKrishna97@users.noreply.github.com>
2023-11-17 10:40:47 -0600

    updated

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+    .then(function (user){
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    })
+    .catch(function(err){
+   return done(err)
+  })
+  })
+  )
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+const accountSchema = new Schema({
+username: String,
+password: String
+});
+accountSchema.plugin(passportLocalMongoose);
+// We export the Schema to avoid attaching the model to the
+// default mongoose connection.
+module.exports = mongoose.model("Account", accountSchema);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +56,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('dotenv').config();
