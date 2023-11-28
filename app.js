12969b01014aed4fb7a5cdd635eb73514e689cd0
Leela Krishna <142929100+LeelaKrishna97@users.noreply.github.com>
2023-11-28 14:07:19 -0600

    register and login

--- app.js
+++ app.js
@@ -6,36 +6,27 @@ var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
     Account.findOne({ username: username })
-    .then(function (user){
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
     })
-    .catch(function(err){
-   return done(err)
-  })
-  })
   )
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
-const accountSchema = new Schema({
-username: String,
-password: String
-});
-accountSchema.plugin(passportLocalMongoose);
-// We export the Schema to avoid attaching the model to the
-// default mongoose connection.
-module.exports = mongoose.model("Account", accountSchema);
+
 
 
 var indexRouter = require('./routes/index');
@@ -61,12 +52,13 @@ app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+}));
+app.use(passport.initialize());
+app.use(passport.session());
   
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 require('dotenv').config();
 const connectionString =
@@ -125,6 +117,14 @@ async function recreateDB() {
 }
 let reseed = true;
 if (reseed) {recreateDB();}
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
